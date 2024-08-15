a53dbd584b59defafd4692f7aaef66f5812ca495
Dean Akpabio <designersuite@outlook.com>
2024-08-15 09:29:52 +0000

    Added Confetti js to start quiz function and show score function in js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// Start Quiz Button
+
+// Function to start the quiz
+function quizStart() {
+    const welcome = document.getElementById("welcome")
+    const quiz = document.getElementById("quizapp");
+    welcome.classList.add("hide");
+    quiz.classList.remove("hide");
+    confettiDrop();
+}
+
+// Add event listener to the button
+document.getElementById("start-quiz").addEventListener("click", quizStart);
+
+// These are the questions for the Quiz
 const questions = [
   {
     question: "In which capital city can the Trevi Fountain be located?",
@@ -215,13 +230,14 @@ function startQuiz(){
   score = 0;
   nextButton.innerHTML = "Next";
   showQuestion();
+  
 }
 
 function showQuestion(){
   resetState();
   let currentQuestion = questions[currentQuestionIndex];
   let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ", " + currentQuestion.
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.
     question;
 
   currentQuestion.answers.forEach(answer => {
@@ -264,15 +280,43 @@ function selectAnswer(e){
   
 }
 
+// Celebratory Function from tsparticles.js
+function confettiDrop() {
+  const duration = 15 * 1000,
+  animationEnd = Date.now() + duration,
+  defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+const interval = setInterval(function() {
+  const timeLeft = animationEnd - Date.now();
+  if (timeLeft <= 0) {
+    return clearInterval(interval);
+  }
+  const particleCount = 50 * (timeLeft / duration);
+  // since particles fall down, start a bit higher than random
+  confetti(
+    Object.assign({}, defaults, {
+      particleCount,
+      origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+    })
+  );
+  confetti(
+    Object.assign({}, defaults, {
+      particleCount,
+      origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+    })
+  );
+}, 250);
+}
+
 function showScore(){
   resetState();
+  confettiDrop();
   questionElement.innerHTML = `You Scored ${score} out of ${questions.length}!`;
   nextButton.innerHTML = "Play Quiz Again";
   nextButton.style.display = "block";
 }
-
-
-
 
 function handleNextButton(){
   currentQuestionIndex++;
